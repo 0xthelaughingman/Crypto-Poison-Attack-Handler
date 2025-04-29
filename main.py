fd77e110955a75b7d3cc30609763e28ab66eee09
TheLaughingMan <theelaughingman@protonmail.com>
2025-04-29 10:38:18 +0530

    Refactor + Fix json items

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flipside import Flipside
 import pandas as pd
 from pandas import DataFrame
+import json
 
 load_dotenv()
 FS_API_KEY = os.getenv("FS_API_KEY")
@@ -42,7 +43,7 @@ def get_transactions(wallet: str):
     ORDER BY BLOCK_ID ASC, INDEX ASC 
     """
 
-    query_result_set = flipside.query(query_sql, ttl_minutes=10)
+    query_result_set = flipside.query(query_sql, ttl_minutes=2)
 
     current_page_number = 1
 
@@ -53,7 +54,7 @@ def get_transactions(wallet: str):
     # we receive the total pages from `get_query_results` given the
     # provided `page_size` (total_pages is dynamically determined by the API
     # based on the `page_size` you provide)
-    total_pages = 10
+    total_pages = 20
 
     # we'll store all the page results in `all_rows`
     all_rows = []
@@ -80,10 +81,9 @@ def check_poison_score(from_address: str, receivers: dict):
     score = [0, 0]
     address_poisoned = None
     for key in receivers:
-        for front_len in range(5, 2, -1):  # front should match a length of 5, 4 or 3 chars minimum...
-            for end_len in range(5, 0, -1):  # end should match a minimum of 1 chars...
+        for front_len in range(5, 2, -1):       # front should match a length of 5, 4 or 3 chars minimum...
+            for end_len in range(5, 0, -1):     # end should match a minimum of 1 chars...
 
-                # print(key[0:4], from_address[0:4], key[::-1][0:4], from_address[::-1][0:4])
                 if key[0:front_len] == from_address[0:front_len] and key[::-1][0:end_len] == from_address[::-1][0:end_len]:
                     score = [front_len, end_len]
                     address_poisoned = key
@@ -100,17 +100,17 @@ def parse_transactions(txns: DataFrame, wallet: str):
     poisoners = {}
     for row in txns.itertuples():
 
-        if row.tx_from == wallet:  # sends
+        if row.tx_from == wallet:       # sends
             to_address = row.tx_to
-            if len(receivers) == 0:  # init receivers
+            if len(receivers) == 0:     # init receivers
                 receivers[to_address] = [row.tx_id, row.block_timestamp]
 
             if to_address not in poisoners and to_address not in receivers:
                 receivers[to_address] = [row.tx_id, row.block_timestamp]
 
-        if row.tx_to == wallet:  # receipts
+        if row.tx_to == wallet:         # receipts
             from_address = row.tx_from
-            if len(senders) == 0:  # init senders dict
+            if len(senders) == 0:       # init senders dict
                 senders[from_address] = [row.tx_id, row.block_timestamp]
 
             if from_address not in senders:
@@ -136,62 +136,79 @@ def parse_transactions(txns: DataFrame, wallet: str):
     return receivers, senders, poisoners
 
 
-def format_output_dfs(txns_input: DataFrame, poisoners: dict):
+def format_output_dfs(txs_input: DataFrame, poisoners: dict):
     if len(poisoners) == 0:
         return [],[]
 
-    poison_attack_txns = []
+    poison_attack_txs = []
 
     for key, value in poisoners.items():
-        df_poision_txns = txns_input[txns_input['tx_from']==key]
-        temp_actual_address = [value[2]] * len(df_poision_txns['tx_id'])
-        temp_score = [value[3]] * len(df_poision_txns['tx_id'])
-        df_poision_txns = df_poision_txns.assign(real_address = temp_actual_address)
-        df_poision_txns = df_poision_txns.assign(poison_score = temp_score)
-        # print(df_poision_txns.to_string())
-        poison_attack_txns.append(df_poision_txns.to_json(orient='records'))
+        df_poison_txs = txs_input[txs_input['tx_from'] == key]
+        temp_actual_address = [value[2]] * len(df_poison_txs['tx_id'])
+        temp_score = [value[3]] * len(df_poison_txs['tx_id'])
+        df_poison_txs = df_poison_txs.assign(real_address = temp_actual_address)
+        df_poison_txs = df_poison_txs.assign(poison_score = temp_score)
+        # print(df_poison_txs.to_string())
+        for index, row in df_poison_txs.iterrows():
+            poison_attack_txs.append(row.to_json())
 
 
-    victim_txns = []
+    victim_txs = []
 
     for key, value in poisoners.items():
-        df_victim_txns = txns_input[txns_input['tx_to'] == key]
-        if len(df_victim_txns) != 0: # don't need this check as poison txns always present, but victim may not ever fall for them!
-            temp_actual_address = [value[2]] * len(df_poision_txns['tx_id'])
-            temp_score = [value[3]] * len(df_poision_txns['tx_id'])
-            df_victim_txns = df_victim_txns.assign(real_address=temp_actual_address)
-            df_victim_txns = df_victim_txns.assign(poison_score=temp_score)
-            # print(df_victim_txns.to_string())
-            victim_txns.append(df_victim_txns.to_json(orient='records'))
+        df_victim_txs = txs_input[txs_input['tx_to'] == key]
+        if len(df_victim_txs) != 0:    # don't need this check as poison txns always present, but victim may not ever fall for them!
+            temp_actual_address = [value[2]] * len(df_victim_txs['tx_id'])
+            temp_score = [value[3]] * len(df_victim_txs['tx_id'])
+            df_victim_txs = df_victim_txs.assign(real_address=temp_actual_address)
+            df_victim_txs = df_victim_txs.assign(poison_score=temp_score)
+            # print(df_victim_txs.to_string())
+            for index, row in df_victim_txs.iterrows():
+                victim_txs.append(row.to_json())
 
-    return poison_attack_txns, victim_txns
+    return poison_attack_txs, victim_txs
+
+def build_json_response(attack_txs:list, victim_txs:list):
+    data = {}
+    if len(attack_txs) == 0:
+        data['poison_attack_count'] = 0
+
+    else:
+        data['poison_attack_count'] = len(attack_txs)
+        data['poison_attack_txs'] = []
+        for item in attack_txs:
+            data['poison_attack_txs'].append(json.loads(item))
+        if len(victim_txs) == 0:
+            data['victim_tx_count'] = 0
+        else:
+            data['victim_tx_count'] = len(victim_txs)
+            data['victim_txs'] = []
+            for item in victim_txs:
+                data['victim_txs'].append(json.loads(item))
+
+    json_data = json.dumps(data)
+    # print(json_data)
+    return json_data
 
 
 if __name__ == "__main__":
-    #wallet = "37eXkm5EQSVuJBtMxreZs725uPVUNAyzcFZwa7yZzQKM"
+
+
     wallet = "5LbwC1ewY3Sca7T8CwzX9wsjvwMAHbdRo6SCQL8j7EWc"
-    txns = None
+
+    txs = None
     try:
-        txns = get_transactions(wallet)
+        txs = get_transactions(wallet)
     except:
         print("Ensure Valid Address/transfer history present.")
 
-    receivers, senders, poisoners = parse_transactions(txns, wallet)
+    receivers, senders, poisoners = parse_transactions(txs, wallet)
 
-    print(len(receivers), len(senders), len(poisoners))
+    # print(len(receivers), len(senders), len(poisoners))
 
-    if len(poisoners)==0:
-        print("No poison attacks detected")
-    else:
-        poison_attack_txns, victim_txns = format_output_dfs(txns, poisoners)
-        print("\nPoison Attack Txns\n")
-        for item in poison_attack_txns:
-            print(item)
+    poison_attack_txs, victim_txs = format_output_dfs(txs, poisoners)
 
-        if len(victim_txns)==0:
-            print("\nWallet Owner hasn't fallen victim")
-        else:
-            print("\nVictim Txns:\n")
-            for item in victim_txns:
-                print(item)
+    json_response = build_json_response(poison_attack_txs, victim_txs)
+
+    print(json_response)
 
